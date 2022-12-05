d3e2baaee07926de483d2cd25f4a04aa2261de22
Yutaka Yamaguchi <yyamaguchi@sixapart.com>
2022-12-05 09:52:58 +0900

    Site updated: 2022-12-05 09:52:57

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,22 @@
-const CACHE_MAIN = 'main1670200471553';
+const CACHE_MAIN = 'main1670201462114';
 const precacheList = [
   
   '/',
+  '/2020/book-programming-typescript.html',
   '/2022/the-two-ways-to-use-autocomplete-attribute.html',
   '/2022/npm-script-warns-permission-errors-in-the-cache-directory.html',
   '/2022/Unexpected-reserved-word-interface-on-jest.html',
   '/2022/webpack-5-compiles-code-with-es2015.html',
-  '/2020/book-programming-typescript.html',
   '/2020/got-100-lighthouse-score-in-all-categories-on-mobile.html',
   '/2020/logical-assignment-operators.html',
   '/2020/fragmenting-flex-layout.html',
   '/2020/flex-layout-algorithm.html',
   '/2020/flex-alignment.html',
+  '/assets/images/bulkan-evcimen-LZg_PuyFPGo-unsplash/bulkan-evcimen-LZg_PuyFPGo-unsplash.svg',
   '/assets/images/craig-manners-vtG_gpJn9OM-unsplash/craig-manners-vtG_gpJn9OM-unsplash.svg',
   '/assets/images/pavan-prasad-nw1slFnKRYE-unsplash/pavan-prasad-nw1slFnKRYE-unsplash.svg',
   '/assets/images/glen-carrie-gsPnE6rMRns-unsplash/glen-carrie-gsPnE6rMRns-unsplash.svg',
   '/assets/images/martin-bennie-xVgHduP61HY-unsplash/martin-bennie-xVgHduP61HY-unsplash.svg',
-  '/assets/images/bulkan-evcimen-LZg_PuyFPGo-unsplash/bulkan-evcimen-LZg_PuyFPGo-unsplash.svg',
   '/assets/images/ben-koorengevel-tpIWpoW3_Wc-unsplash/ben-koorengevel-tpIWpoW3_Wc-unsplash.svg',
   '/assets/images/cath-smith-PF5_cqOQFT4-unsplash/cath-smith-PF5_cqOQFT4-unsplash.svg',
   '/assets/images/tomas-malik-zlSzh2FP7LY-unsplash/tomas-malik-zlSzh2FP7LY-unsplash.svg',

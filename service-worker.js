59a0eac5d10927f2383e555b1d8064bce2600014
Yutaka Yamaguchi <yyamaguchi@sixapart.com>
2022-12-21 12:24:49 +0900

    Site updated: 2022-12-21 12:24:48

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,8 @@
-const CACHE_MAIN = 'main1670720065316';
+const CACHE_MAIN = 'main1671592984263';
 const precacheList = [
   
   '/',
+  '/2022/how-to-align-the-legen-in-the-fieldset.html',
   '/2022/autocomplete-fields-have-an-appropriate-control-group.html',
   '/2022/the-meaning-of-autocomplete-in-the-accessibility.html',
   '/2020/book-programming-typescript.html',
@@ -11,7 +12,7 @@ const precacheList = [
   '/2022/webpack-5-compiles-code-with-es2015.html',
   '/2020/got-100-lighthouse-score-in-all-categories-on-mobile.html',
   '/2020/logical-assignment-operators.html',
-  '/2020/fragmenting-flex-layout.html',
+  '/assets/images/neha-maheen-mahfin-3szSK-40AWM-unsplash/neha-maheen-mahfin-3szSK-40AWM-unsplash.svg',
   '/assets/images/bingqi-huang-wpi1d9-o4vo-unsplash/bingqi-huang-wpi1d9-o4vo-unsplash.svg',
   '/assets/images/srikanta-h-u-Ju0DX6yhcBE-unsplash/srikanta-h-u-Ju0DX6yhcBE-unsplash.svg',
   '/assets/images/bulkan-evcimen-LZg_PuyFPGo-unsplash/bulkan-evcimen-LZg_PuyFPGo-unsplash.svg',
@@ -21,7 +22,6 @@ const precacheList = [
   '/assets/images/martin-bennie-xVgHduP61HY-unsplash/martin-bennie-xVgHduP61HY-unsplash.svg',
   '/assets/images/ben-koorengevel-tpIWpoW3_Wc-unsplash/ben-koorengevel-tpIWpoW3_Wc-unsplash.svg',
   '/assets/images/cath-smith-PF5_cqOQFT4-unsplash/cath-smith-PF5_cqOQFT4-unsplash.svg',
-  '/assets/images/tomas-malik-zlSzh2FP7LY-unsplash/tomas-malik-zlSzh2FP7LY-unsplash.svg',
   '/css/prism.css',
   '/css/global_async.css',
 ];

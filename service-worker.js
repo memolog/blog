9960670f2a54826fd1be6726ae9fb2dfb9fbf76c
Yutaka Yamaguchi <yyamaguchi@sixapart.com>
2024-11-04 07:47:44 +0900

    Site updated: 2024-11-04 07:47:44

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,7 @@
-const CACHE_MAIN = 'main1730673910909';
+const CACHE_MAIN = 'main1730593550620';
 const precacheList = [
   
   '/',
-  '/2024/battle-of-nagashino.html',
   '/2024/beyond-weird.html',
   '/2024/introduction-to-animal-psychology.html',
   '/2024/origin-of-samurai-goverment.html',
@@ -12,7 +11,7 @@ const precacheList = [
   '/2022/webpack-5-compiles-code-with-es2015.html',
   '/2022/Unexpected-reserved-word-interface-on-jest.html',
   '/2022/the-two-ways-to-use-autocomplete-attribute.html',
-  '/assets/images/generated-image-of-battle-of-nagashino/generated-image-of-battle-of-nagashino.svg',
+  '/2020/got-100-lighthouse-score-in-all-categories-on-mobile.html',
   '/assets/images/generated-image-of-beyond-weird/generated-image-of-beyond-weird.svg',
   '/assets/images/generated-image-of-introduction-animal-psychology/generated-image-of-introduction-animal-psychology.svg',
   '/assets/images/generated-image-of-origin-of-samurai/generated-image-of-origin-of-samurai.svg',
@@ -22,6 +21,7 @@ const precacheList = [
   '/assets/images/martin-bennie-xVgHduP61HY-unsplash/martin-bennie-xVgHduP61HY-unsplash.svg',
   '/assets/images/glen-carrie-gsPnE6rMRns-unsplash/glen-carrie-gsPnE6rMRns-unsplash.svg',
   '/assets/images/craig-manners-vtG_gpJn9OM-unsplash/craig-manners-vtG_gpJn9OM-unsplash.svg',
+  '/assets/images/ben-koorengevel-tpIWpoW3_Wc-unsplash/ben-koorengevel-tpIWpoW3_Wc-unsplash.svg',
   '/css/prism.css',
   '/css/global_async.css',
 ];
